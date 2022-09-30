00563d203e652f2db90fe66408ff24a4d93e3479
Aakash Arumugam <81348470+Aakash8302-dev@users.noreply.github.com>
2022-10-01 00:41:36 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import firebase from "firebase/compat/app";
 import 'firebase/compat/firestore'
   
 const firebaseConfig = {
-    apiKey: "AIzaSyAkANs98sDIhl2PV3RNCTzrFvXWEnhYcCc",
-    authDomain: "threatdetecion.firebaseapp.com",
-    databaseURL: "https://threatdetecion-default-rtdb.firebaseio.com",
-    projectId: "threatdetecion",
-    storageBucket: "threatdetecion.appspot.com",
-    messagingSenderId: "399294282193",
-    appId: "1:399294282193:web:2fbc2961f9a844a465c8f0",
-    measurementId: "G-M7GEKF2KSC"
+  #YOUR_CONFIG_KEYS
   };
   
 const firebaseApp = firebase.initializeApp(firebaseConfig);
